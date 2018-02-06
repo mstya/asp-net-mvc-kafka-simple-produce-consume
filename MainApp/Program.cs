9aeec9cfa40e7b3daec892fa4e73824ebacb6454
Viacheslav Yankov <slava.brigida@nixsolutions.com>
2018-02-06 16:22:44 +0200

    added simple example

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
 
-namespace MainApp
+namespace KafkaProducer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new Dictionary<string, object>
+            {
+                {"bootstrap.servers", "localhost:9092"}
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                string text = null;
+                var rand = new Random();
+                //while (text != "exit")
+                //{
+                //    Console.Write("Write: ");
+                //    text = Console.ReadLine();
+                //    producer.ProduceAsync("hello-topic", null, text);
+                //}
+
+                while (true)
+                {
+                    string val = rand.Next(0, 100).ToString();
+                    Console.WriteLine(val);
+                    producer.ProduceAsync("hello-topic", null, val);
+
+                    Thread.Sleep(100);
+                }
+
+                producer.Flush(100);
+            }
         }
     }
 }
